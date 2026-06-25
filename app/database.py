c7441fe711792de221ccb09a8ffae3bf044b44a8
Sumaya Abdi <sumeayaabdi@gmail.com>
2026-06-25 21:55:27 +0300

    Added my registration app

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 import sqlite3
-from contextlib import contextmanager  ##helps and handles the connection to the database
+from contextlib import contextmanager  
 
 sqlite_file_name = "school.db"
 
@@ -8,12 +8,12 @@ def get_connection():
     connection = sqlite3.connect(sqlite_file_name)
     connection.row_factory = sqlite3.Row
     try:
-        yield connection   ##is like return
+        yield connection   
         connection.commit()
     finally:
         connection.close()
 
-def create_table(): ##create table
+def create_table(): 
     with get_connection() as connection:
         connection.execute('''CREATE TABLE IF NOT EXISTS students(
                            id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -41,36 +41,36 @@ def create_table(): ##create table
                            field_of_course TEXT NOT NULL )''')
         
         
-def add_student(id,name,age,email,country, id_number): ##insert data
+def add_student(id,name,age,email,country, id_number): 
     with get_connection() as connection:
         connection.execute(
             'INSERT INTO students(id,name,age,email,country,id_number) VALUES (?,?,?,?,?,?)',
             (id,name,age,email,country,id_number)        
         )
 
-def add_teacher(id,name,age,course_teaching,experience, education_level): ##insert data
+def add_teacher(id,name,age,course_teaching,experience, education_level): 
     with get_connection() as connection:
         connection.execute(
             'INSERT INTO teachers(id,name,age,course_teaching,experience, education_level) VALUES (?,?,?,?,?,?)',
             (id,name,age,course_teaching,experience, education_level)  
         )
 
-def add_course(course_id,name_of_course,subtopics,duration_of_course,field_of_course): ##insert data
+def add_course(course_id,name_of_course,subtopics,duration_of_course,field_of_course): 
     with get_connection() as connection:
         connection.execute(
              'INSERT INTO courses(course_id,name_of_course,subtopics,duration_of_course,field_of_course) VALUES (?,?,?,?,?)',
             (course_id,name_of_course,subtopics,duration_of_course,field_of_course)   
         )
 
-def get_students(): ##get the students
+def get_students(): 
     with get_connection() as connection:
         return connection.execute('SELECT * FROM students').fetchall()
     
-def get_teachers(): ##get the teachers
+def get_teachers(): 
     with get_connection() as connection:
         return connection.execute('SELECT * FROM teachers').fetchall()
     
-def get_courses(): ##get the courses
+def get_courses(): 
     with get_connection() as connection:
         return connection.execute('SELECT * FROM courses').fetchall()
     
